7dbcbc6ce1e583cfb7c389a0d5d2278bce68e0d9
Mohammad Zeeshan Jawed <89350024+zeeshan8281@users.noreply.github.com>
2022-01-12 00:24:39 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/*Create any project that sends messages! I could be a simple hello, world or a full chat application*/
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class LHD1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hey There!, Have a Nice Day");
+	}
+}
